397d965b9472b7399d94a5d2f4914a7e9dc5e167
shanksms <sriv.srivastava@gmail.com>
2022-03-23 18:29:45 +0530

    numpy file

--- oreilly_probability_course_examples/binomial_random_variable.py
+++ oreilly_probability_course_examples/binomial_random_variable.py
@@ -7,12 +7,12 @@ def binomial_trial(n, p = 0.5):
 
 
 if __name__ == '__main__':
-    N = 10000
+    N = 1000
     n = 1000
     p = 0.5
     B = np.zeros(N)
     for i in range(N):
         B[i] = binomial_trial(n, p)
     plt.hist(B, density=True, bins=20)
-    sns.kdeplot(B, shade=True)
+    #sns.kdeplot(B, shade=True)
     plt.show()
