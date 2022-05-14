26778cab4531c0c1b9aac235173170cb55a53497
shanksms <sriv.srivastava@gmail.com>
2022-05-14 19:22:14 +0530

    matplot lib example

--- matplotlib_examples/fifa.py
+++ matplotlib_examples/fifa.py
@@ -5,6 +5,7 @@ import pandas as pd
 class Fifa:
     def __init__(self):
         self.fifa_df = pd.read_csv('fifa.csv')
+        self.fifa_df.loc[:, 'Weight'] = self.fifa_df.loc[:, 'Weight'].str.replace('lbs', '')
 
     def get_fifa_df(self):
         return self.fifa_df
@@ -20,15 +21,19 @@ def hist_plot(fifa_df: pd.DataFrame):
     plt.show()
 
 
-def pie_plot(fifa_df: pd.DataFrame):
+def pie_plot_foot_preference(fifa_df: pd.DataFrame):
     left = fifa_df.loc[fifa_df['Preferred Foot'] == 'Left', :].shape[0]
     right = fifa_df.loc[fifa_df['Preferred Foot'] == 'Right', :].shape[0]
-    plt.pie([left, right])
+    labels = ['Left', 'Right']
+    colors = ['#42d1f5', '#f5425d']
+    plt.pie([left, right], labels=labels, colors=colors, autopct='%.2f %%')
+    plt.title('Foot preference of Fifa players')
     plt.show()
+
 
 
 if __name__ == '__main__':
     fifa_df = Fifa().get_fifa_df()
     hist_plot(fifa_df)
-    pie_plot(fifa_df)
+    pie_plot_foot_preference(fifa_df)
 
