d2c4d7dae5739bbb8245448df2a430209e4fe3c8
shanksms <sriv.srivastava@gmail.com>
2022-05-14 22:29:29 +0530

    matplot lib example

--- matplotlib_examples/fifa.py
+++ matplotlib_examples/fifa.py
@@ -6,6 +6,7 @@ class Fifa:
     def __init__(self):
         self.fifa_df = pd.read_csv('fifa.csv')
         self.fifa_df.loc[:, 'Weight'] = self.fifa_df.loc[:, 'Weight'].str.replace('lbs', '')
+        self.fifa_df = self.fifa_df.astype({'Weight': 'float'})
 
     def get_fifa_df(self):
         return self.fifa_df
@@ -31,9 +32,32 @@ def pie_plot_foot_preference(fifa_df: pd.DataFrame):
     plt.show()
 
 
+def pie_plot_weights(fifa_df: pd.DataFrame) -> None:
+    plt.style.use('ggplot')
+    light_count = fifa_df[fifa_df['Weight'] < 125].shape[0]
+    medium_count = fifa_df[(fifa_df['Weight'] >= 125) & (fifa_df['Weight'] < 175)].shape[0]
+    heavy_count = fifa_df[(fifa_df['Weight'] >= 175)].shape[0]
+    weights = [light_count, medium_count, heavy_count]
+    labels = ['Light', 'Medium', 'Heavy']
+    plt.pie(weights, labels=labels, autopct='%.2f %%', pctdistance=0.8)
+    plt.show()
+
+
+def fifa_score_box_plot(fifa_df: pd.DataFrame) -> None:
+    plt.style.use('ggplot')
+    barcelona = fifa_df[fifa_df['Club'] == 'FC Barcelona']['Overall']
+    madrid = fifa_df[fifa_df['Club'] == 'Real Madrid']['Overall']
+    labels = ['FC Barcelona', 'Real Madrid']
+    plt.boxplot([barcelona, madrid], labels=labels)
+    plt.title('Professional Soccer comparison')
+    plt.ylabel('FIFA overall rating')
+    plt.show()
+
 
 if __name__ == '__main__':
     fifa_df = Fifa().get_fifa_df()
     hist_plot(fifa_df)
     pie_plot_foot_preference(fifa_df)
+    pie_plot_weights(fifa_df)
+    fifa_score_box_plot(fifa_df)
 
