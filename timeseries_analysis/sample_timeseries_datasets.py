f2985febad5421c035e463bba99beecf0d636adc
shanksms <sriv.srivastava@gmail.com>
2022-09-11 15:07:29 +0530

    time series analysis

--- timeseries_analysis/sample_timeseries_datasets.py
+++ timeseries_analysis/sample_timeseries_datasets.py
@@ -16,7 +16,33 @@ def gdp_data():
 
 def cdc_data():
     ILI = pd.read_csv('CDC.csv')
+    ILI = ILI.loc[:, ['Percent of Deaths Due to Pneumonia and Influenza', 'Expected', 'Threshold']]
+    ax = ILI.plot()
+    plt.show()
+
+def sun_activity_data():
+    sun_activity = pd.read_csv('sun.csv')
+    ax = sun_activity.plot(x='YEAR', y='SUNACTIVITY', legend=False)
+    ax.set_xlabel('Year')
+    ax.set_ylabel('Sun spot activity')
+    plt.show()
+
+
+def djia_data():
+    djia = pd.read_csv('DJIA.csv', parse_dates=['DATE'], na_values='.')
+    ax = djia.plot(x='DATE', y='DJIA')
+    plt.show()
+
+
+def airline_data():
+    airline = pd.read_csv('international-airline-passengers.csv', parse_dates=['Month'], sep=';')
+    airline = airline.set_index('Month')
+    ax = airline.plot(legend=False)
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Passengers')
+    plt.show()
+
 
 
 if __name__ == '__main__':
-    gdp_data()
+    airline_data()
